19924060521b69788bbd4570ef3537f892296f8a
Lameck <lameck2@hotmail.com>
2019-08-17 12:57:30 -0300

    feat: Create and upload hyper config file

---
+++ hyper.js
@@ -0,0 +1,151 @@
+// Future versions of Hyper may add additional config options,
+// which will not automatically be merged into this file.
+// See https://hyper.is#cfg for all currently supported options.
+
+module.exports = {
+	config: {
+		// choose either `'stable'` for receiving highly polished,
+		// or `'canary'` for less polished but more frequent updates
+		updateChannel: 'stable',
+
+		// default font size in pixels for all tabs
+		fontSize: 12,
+
+		// font family with optional fallbacks
+		fontFamily: 'Menlo, "Fira Code", Consolas, "Fira Code", monospace',
+
+		// default font weight: 'normal' or 'bold'
+		fontWeight: 'normal',
+
+		// font weight for bold characters: 'normal' or 'bold'
+		fontWeightBold: 'bold',
+
+		// line height as a relative unit
+		lineHeight: 1,
+
+		// letter spacing as a relative unit
+		letterSpacing: 0,
+
+		// terminal cursor background color and opacity (hex, rgb, hsl, hsv, hwb or cmyk)
+		cursorColor: 'rgba(248,28,229,0.8)',
+
+		// terminal text color under BLOCK cursor
+		cursorAccentColor: '#000',
+
+		// `'BEAM'` for |, `'UNDERLINE'` for _, `'BLOCK'` for █
+		cursorShape: 'BEAM',
+
+		// set to `true` (without backticks and without quotes) for blinking cursor
+		cursorBlink: true,
+
+		// color of the text
+		foregroundColor: '#fff',
+
+		// terminal background color
+		// opacity is only supported on macOS
+		backgroundColor: '#000',
+
+		// terminal selection color
+		selectionColor: 'rgba(248,28,229,0.3)',
+
+		// border color (window, tabs)
+		borderColor: '#333',
+
+		// custom CSS to embed in the main window
+		css: `.header_header {
+			background: transparent!important;
+		}
+		.tab_tab {
+			border: 0;
+		}
+		.tab_active::before {
+			border-bottom: 2px solid rgba(255,255,255,.5);
+		}
+		.tab_hasActivity {
+			color: #42a1e4;
+		}
+		.tab_hasActivity:hover {
+			color: #96d4e4;
+		}
+`,
+
+		termCSS: '',
+
+		showHamburgerMenu: false,
+
+		showWindowControls: false,
+
+		padding: '0px 5px',
+
+		colors: {
+			black: '#000000',
+			red: '#C51E14',
+			green: '#1DC121',
+			yellow: '#C7C329',
+			blue: '#0A2FC4',
+			magenta: '#C839C5',
+			cyan: '#20C5C6',
+			white: '#C7C7C7',
+			lightBlack: '#686868',
+			lightRed: '#FD6F6B',
+			lightGreen: '#67F86F',
+			lightYellow: '#FFFA72',
+			lightBlue: '#6A76FB',
+			lightMagenta: '#FD7CFC',
+			lightCyan: '#68FDFE',
+			lightWhite: '#FFFFFF'
+		},
+
+		shell: '',
+
+		// for setting shell arguments (i.e. for using interactive shellArgs: `['-i']`)
+		// by default `['--login']` will be used
+		shellArgs: ['--login'],
+
+		// for environment variables
+		env: {},
+
+		// set to `false` for no bell
+		bell: 'SOUND',
+
+		// if `true` (without backticks and without quotes), selected text will automatically be copied to the clipboard
+		copyOnSelect: true,
+
+		// if `true` (without backticks and without quotes), hyper will be set as the default protocol client for SSH
+		defaultSSHApp: true,
+
+		// if `true` (without backticks and without quotes), on right click selected text will be copied or pasted if no
+		// selection is present (`true` by default on Windows and disables the context menu feature)
+		quickEdit: false,
+
+		// choose either `'vertical'`, if you want the column mode when Option key is hold during selection (Default)
+		// or `'force'`, if you want to force selection regardless of whether the terminal is in mouse events mode
+		// (inside tmux or vim with mouse mode enabled for example).
+		macOptionSelectionMode: 'vertical',
+
+		// URL to custom bell
+		// bellSoundURL: 'http://example.com/bell.mp3',
+
+		// Whether to use the WebGL renderer. Set it to false to use canvas-based
+		// rendering (slower, but supports transparent backgrounds)
+		webGLRenderer: true
+
+		// for advanced config flags please refer to https://hyper.is/#cfg
+	},
+
+	plugins: [
+		'hyper-statusline',
+		'hyper-tabs-enhanced',
+		'hyper-dark-scrollbar',
+		'hypercwd',
+		'hyperocean'
+	],
+	scrollback: 100000000,
+
+	localPlugins: [],
+
+	keymaps: {
+		// Example
+		// 'window:devtools': 'ctrl+alt+o'
+	}
+};
